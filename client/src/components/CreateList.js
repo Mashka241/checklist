d5c0e43b76d45c333867d458f582977b63029236
Maria Ivanova <mariaivanova@microsoft.com>
2025-03-05 15:38:30 +0100

    add create list form

--- client/src/components/CreateList.js
+++ client/src/components/CreateList.js
@@ -1,57 +1,83 @@
 import { useState } from 'react';
 
 function CreateListForm() {
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [currentTodo, setCurrentTodo] = useState('');
+  const [todos, setTodos] = useState([]);
 
-    const onSaveHandler = async () => {
-        const url = 'http://localhost:8080/lists';
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ title, description })
-        };
-        const response = await fetch(url, requestOptions);
-        console.log('response', response);
+  const onSaveHandler = async () => {
+    if (!title) {
+      return;
+    }
+    const url = 'http://localhost:8080/lists';
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ title, description, todos })
     };
+    const response = await fetch(url, requestOptions);
+    setTitle('');
+    setDescription('');
+    setTodos([]);
+    console.log('response', response);
+  };
 
-    return (
-        <div>
-            <div>
-                <label htmlFor='title'>Title: </label>
-                <input type='text' value={title} id='title' onChange={e => setTitle(e.target.value)} required />
+  const onAddHandler = () => {
+    setTodos([...todos, currentTodo]);
+    setCurrentTodo('');
+  };
 
-                <label htmlFor='description'>Description: </label>
-                <input type='text' value={description} id='description' onChange={e => setDescription(e.target.value)} />
+  return (
+    <div>
+      <div className='create-list-container'>
+        <label htmlFor='title'>Title: </label>
+        <input type='text' value={title} id='title' onChange={e => setTitle(e.target.value)} required />
 
-                <button onClick={onSaveHandler}>Save!</button>
-            </div>
-        </div>
-    );
+        <label htmlFor='description'>Description: </label>
+        <input type='text' value={description} id='description' onChange={e => setDescription(e.target.value)} />
+
+        <label htmlFor='items'>To do: </label>
+        {!!todos.length && 
+            <ul>
+              {todos.map(el => {
+                return (
+                  <li key={el}>{el}</li>
+                );
+              })}
+            </ul>
+        }
+        <input type='text' id='items' value={currentTodo} onChange={e => setCurrentTodo(e.target.value)}></input>
+        <button onClick={onAddHandler}>Add todo</button>
+
+        <button onClick={onSaveHandler}>Save!</button>
+      </div>
+    </div>
+  );
 }
 
 export function CreateList() {
-    const [showCreateForm, setShowCreateForm] = useState(false);
+  const [showCreateForm, setShowCreateForm] = useState(false);
 
-    const addListClickHandler = () => {
-        setShowCreateForm(true);
-    };
+  const addListClickHandler = () => {
+    setShowCreateForm(true);
+  };
 
-    const closeClickHandler = () => {
-        setShowCreateForm(false);
-    };
+  const closeClickHandler = () => {
+    setShowCreateForm(false);
+  };
 
-    return (
-        <div>
-            {
-                showCreateForm ? 
-                    <div>
-                        <CreateListForm/>
-                        <button onClick={closeClickHandler}>Close</button>
-                    </div> 
-                    : 
-                    <button onClick={addListClickHandler}>Create new checklist</button>
-            }
-        </div>
-    );
+  return (
+    <div>
+      {
+        showCreateForm ? 
+          <div>
+            <CreateListForm/>
+            <button onClick={closeClickHandler}>Close</button>
+          </div> 
+          : 
+          <button onClick={addListClickHandler}>Create new checklist</button>
+      }
+    </div>
+  );
 }
--- client/src/components/Lists.js
+++ client/src/components/Lists.js
@@ -3,46 +3,32 @@ import { NavLink } from 'react-router-dom';
 import { CreateList } from './CreateList';
 
 export function Lists() {
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
   
-    const fetchLists = async () => {
-      const url = 'http://localhost:8080/lists';
-      const response = await fetch(url);
-      const lists = await response.json();
-      console.log('lists:', lists);
-      setData(lists);
-    };
+  const fetchLists = async () => {
+    const url = 'http://localhost:8080/lists';
+    const response = await fetch(url);
+    const lists = await response.json();
+    console.log('lists:', lists);
+    setData(lists);
+  };
     
-    useEffect(() => {
-      fetchLists();
-    }, []);
+  useEffect(() => {
+    fetchLists();
+  }, []);
   
-    // const deleteClickHandler = async () => {
-    //   console.log('deleting list 2');
-    //   const id = 2;
-    //   const url = `http://localhost:8080/lists/${id}`;
-    //   const requestOptions = {
-    //     method: 'DELETE',
-    //     headers: { 'Content-Type': 'application/json' },
-    //   };
-    //   const response = await fetch(url, requestOptions);
-    //   fetchLists();
-    //   console.log('response', response);
-    // };
-  
-    return(
-      <div>
-        <NavLink to='/' end>Home</NavLink>
-        <h1>Lists</h1>
-        <ul>{data.map(el => {
-          return (
-            <NavLink key={el.id} to={`/lists/${el.id}`}>
-              <li>{el.title}</li>
-            </NavLink>
-          );
-        })}</ul>
-        <CreateList/>
-        {/* <button onClick={deleteClickHandler}>delete second list</button> */}
-      </div>
-    );
-  }
+  return(
+    <div>
+      <NavLink to='/' end>Home</NavLink>
+      <h1>Lists</h1>
+      <ul>{data.map(el => {
+        return (
+          <NavLink key={el.id} to={`/lists/${el.id}`}>
+            <li>{el.title}</li>
+          </NavLink>
+        );
+      })}</ul>
+      <CreateList/>
+    </div>
+  );
+}
